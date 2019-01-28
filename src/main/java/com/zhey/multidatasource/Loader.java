0e59bb107432c0c7eb376d025fceeae6ae6fcf19
zhey <zhaoenyang@126.com>
2019-01-28 15:54:07 +0800

    添加注释

--- src/main/java/com/zhey/multidatasource/Loader.java
+++ src/main/java/com/zhey/multidatasource/Loader.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.zhey.multidatasource;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -5,6 +20,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 /**
+ * 引入数据库、事务管理和mybatis配置
+ *
  * @author zhey
  */
 @Configuration
--- src/main/java/com/zhey/multidatasource/MultiMybatisProperties.java
+++ src/main/java/com/zhey/multidatasource/MultiMybatisProperties.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.zhey.multidatasource;
 
 import org.apache.ibatis.session.Configuration;
@@ -14,6 +29,8 @@ import java.util.List;
 import java.util.Properties;
 
 /**
+ * 使用mybatis的MybatisProperties
+ *
  * @author zhey
  */
 public class MultiMybatisProperties {
