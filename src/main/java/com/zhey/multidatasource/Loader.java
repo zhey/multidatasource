14a2c119ccbe4267540f026c3e9dffb49ea2d6f7
zhey <zhaoenyang@126.com>
2019-01-28 15:35:00 +0800

    引入tomcat、事务以及mybatis配置

--- src/main/java/com/zhey/multidatasource/Loader.java
+++ src/main/java/com/zhey/multidatasource/Loader.java
@@ -1,31 +1,14 @@
 package com.zhey.multidatasource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-
-import javax.annotation.PostConstruct;
 
 /**
  * @author zhey
  */
 @Configuration
 @EnableConfigurationProperties(MultiDatasourceProperties.class)
-@Import(MultiDatasourceLoader.class)
+@Import({MultiDatasourceLoader.class, TransactionManagerLoader.class, MultiMybatisLoader.class})
 public class Loader {
-    @Autowired
-    private MultiDatasourceProperties properties;
-    @Autowired
-    private ApplicationContext applicationContext;
-    @Autowired(required = false)
-    private MultiDatasourceAutoConfigurationInterface datasourceAutoConfiguration;
-
-    @PostConstruct
-    public void init() {
-        datasourceAutoConfiguration.setApplicationContext(this.applicationContext);
-        datasourceAutoConfiguration.setProperties(this.properties);
-        datasourceAutoConfiguration.init();
-    }
 }
